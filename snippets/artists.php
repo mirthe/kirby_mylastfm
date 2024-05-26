cf9dda3e94d8f2bb5ce5965e8f75e8f97d498cfb
Mirthe Valentijn <me@mirthe.org>
2024-05-26 16:39:35 +0200

    Cleanup

--- snippets/artists.php
+++ snippets/artists.php
@@ -1,16 +1,31 @@
 <?php include('getartists.php'); ?>
 
-<!-- TODO get image from Wikipedia? Last.fm is not offering one for bands.. -->
-
 <div class="masonry">
 <?php foreach($rss->topartists->artist as $item): ?>
 
+    <?php // get image from Wikipedia. Last.fm is not offering one for bands..
+    $article_name = str_replace(' ','_',$item->name);
+    $url = "https://en.wikipedia.org/api/rest_v1/page/summary/".$article_name."?limit=1";
+    // TODO add +(band) if result is 0 and try again? 
+
+    $ch = curl_init( $url );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    $output = curl_exec( $ch );
+    curl_close( $ch );
+
+    $wikipage = json_decode($output,true);
+    // print_r($wikipage); ?>
+
     <div class="block block--artist">
 
-        <?php if (!empty($item->image[3])): ?>
-            <a href="<?= $item->url ?>"><img class="block--img" src="<?= $item->image[3] ?>" alt="Cover <?= $item->artist->name ?> - <?= $item->name ?>"></a>
-        <?php else: ?>
+        <?php if (empty($wikipage) || $wikipage['title'] == 'Not found.' || !key_exists('thumbnail',$wikipage)): ?>
             <a href="<?= $item->url ?>" class="block--fallback"></a>
+        <?php else: ?>
+            <a href="<?= $item->url ?>"
+                    title="<?= $item->name ?>">
+                    <img class="block--img" width="300" height="250"
+                        src="<?= $wikipage['thumbnail']['source'] ?>" alt="<?= $item->artist ?>">
+            </a>
         <?php endif ?>
         
         <div class="block--body">
--- snippets/tracks-imgs.php
+++ snippets/tracks-imgs.php
@@ -7,13 +7,20 @@ foreach($rss->recenttracks->track as $item):
       
       <div class="block block--compact block--track">
         <?php if (!empty($item->image[3])): ?>
-            <a href="<?= $item->url ?>"><img class="block--img" width="300" height="300" src="<?= $item->image[3] ?>" alt="Cover <?= $item->artist->name ?> - <?= $item->name ?>"></a>
+            <a href="<?= $item->url ?>" 
+              title="<?= $item->artist ?> - <?= $item->name ?>">
+              <img class="block--img"
+                width="300" height="300"
+                src="<?= $item->image[3] ?>" alt="">
+          </a>
         <?php else: ?>
-            <a href="<?= $item->url ?>" class="block--fallback"></a>
+            <a href="<?= $item->url ?>" 
+              title="<?= $item->artist ?> - <?= $item->name ?>"
+              class="block--fallback"></a>
         <?php endif ?>
-          <div class="block__overlay">
-            <?= $item->artist ?><br><?= $item->name ?>
-          </div>
+        <div class="block__overlay">
+          <?= $item->artist ?><br><?= $item->name ?>
+        </div>
       </div>
 
 <?php $counter++;
